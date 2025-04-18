2c183d795fa9c6daad72f7d0fa09892eb8c239c9
RakhiGpt <rakhikumaricr1405@gmail.com>
2025-04-18 20:42:32 +0530

    addind new2 file

--- test_case.py
+++ test_case.py
@@ -6,3 +6,5 @@ def test_add_beneficiart():
     result = add_beneficiary("R001" , "thrisha")
     assert result == "beneficiary added" , f"expected 'beneficiary added' , but got {result}"
     assert beneficiaries == {"R001": "thrisha"},  f"expected {{'R001':'thrisha'}} , but got {beneficiaries}"
+
+    test_add_beneficiart()
